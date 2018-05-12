f05c4df4f955ff25b3be77ed0f926731dfeb40c9
Doan Bui <congdoan6997@outlook.com>
2018-05-13 01:48:50 +0300

    add migration and repository

--- TeduShop.Model/Models/OrderDetail.cs
+++ TeduShop.Model/Models/OrderDetail.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TeduShop.Model.Models
 {
@@ -12,14 +7,19 @@ namespace TeduShop.Model.Models
     public class OrderDetail
     {
         [Key]
+        [Column(Order = 1)]
         public int OrderID { get; set; }
+
         [Key]
+        [Column(Order = 2)]
         public int ProductID { get; set; }
+
         public int Quantitty { get; set; }
+
         [ForeignKey("OrderID")]
         public virtual Order Order { get; set; }
+
         [ForeignKey("ProductID")]
         public virtual Product Product { get; set; }
-
     }
 }
