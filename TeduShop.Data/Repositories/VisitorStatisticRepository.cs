f77988b974af077c08595a98e86833433deb7fc7
Congdoan97 <congdoan6997@gmail.com>
2018-05-18 11:05:10 +0300

    add more repositories for other tables

---
+++ TeduShop.Data/Repositories/VisitorStatisticRepository.cs
@@ -0,0 +1,16 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IVisitirStatisticRepository
+    {
+    }
+
+    public class VisitorStatisticRepository : RepositoryBase<VisitorStatistic>, IVisitirStatisticRepository
+    {
+        public VisitorStatisticRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
