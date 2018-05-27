8f03d5d8d9f3fcaf04d01809b42f274d6c2abf82
Congdoan97 <congdoan6997@gmail.com>
2018-05-27 14:40:15 +0300

    scope in angular

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,8 @@
+﻿var myApp = angular.module("myModule", []);
+
+myApp.controller('myController', function ($scope) {
+    $scope.message = 'Hola!';
+});
+myApp.controller('myController2', function ($scope) {
+    $scope.message = 'day la cong doan';
+});
