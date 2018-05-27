5fa736414f7b0f76f1035fe9868bc565b359849f
Congdoan97 <congdoan6997@gmail.com>
2018-05-28 01:35:27 +0300

    service in angular

--- TeduShop.Web/Scripts/spa/app.js
+++ TeduShop.Web/Scripts/spa/app.js
@@ -1,8 +1,29 @@
-﻿var myApp = angular.module("myModule", []);
+﻿/// <reference path="../plugins/angular/angular.js" />
 
-myApp.controller('myController', function ($scope) {
-    $scope.message = 'Hola!';
-});
-myApp.controller('myController2', function ($scope) {
-    $scope.message = 'day la cong doan';
-});
+var myApp = angular.module("myModule", []);
+
+myApp.controller('schoolController', schoolController);
+myApp.service('Validitor', Validitor);
+
+schoolController.$inject = ['$scope', 'Validitor'];
+
+function schoolController($scope, Validitor) {
+    $scope.num = 1;
+    $scope.checkNumber = function () {
+        $scope.message = Validitor.checkNumber($scope.num);
+    }
+   
+}
+function Validitor($window) {
+    return {
+        checkNumber: checkNumber
+    }
+
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return "This is even";
+        } else {
+            return "This is odd";
+        }
+    };
+};
